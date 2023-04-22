7f72e30ec911734bb2d5b51476d4a6187d8245fd
bmay <bloodymay456@gmail.com/ssh>
2023-04-22 12:53:30 +0300

    task1_formatted

--- HW2_tasks/task1/Program.java
+++ HW2_tasks/task1/Program.java
@@ -6,30 +6,29 @@
 // Параметры для фильтрации: {"name":"Ivanov", "country":"Russia", "city":"Moscow", "age":"null"}
 
 package HW2_tasks.task1;
+
 import java.util.HashMap;
 import java.util.Map;
+
 public class Program {
-    public static String getQuery(Map<String, String> params)
-    {
+    public static String getQuery(Map<String, String> params) {
         StringBuilder request = new StringBuilder();
-        for (Map.Entry<String,String> pair : params.entrySet())
-        {
-            if (pair.getValue() != "null")
-            {
-                request.append(pair.getKey() +" = '" + pair.getValue()+"' and ");
+        for (Map.Entry<String, String> pair : params.entrySet()) {
+            if (pair.getValue() != "null") {
+                request.append(pair.getKey() + " = '" + pair.getValue() + "' and ");
             }
         }
-        request.delete(request.toString().length()-5,request.toString().length());
+        request.delete(request.toString().length() - 5, request.toString().length());
         return request.toString();
     }
-	
-    public static void main(String[] args)  {
-        Map <String,String> param = new HashMap<String, String>();
-        param.put("name","Ivanov");
-        param.put("country","Russia");
-        param.put("city","Moscow");
-        param.put("age","null");
+
+    public static void main(String[] args) {
+        Map<String, String> param = new HashMap<String, String>();
+        param.put("name", "Ivanov");
+        param.put("country", "Russia");
+        param.put("city", "Moscow");
+        param.put("age", "null");
         System.out.println(getQuery(param));
-        
+
     }
 }
